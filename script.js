d847b31facaed5079c367fea3113bebb56eae807
Jo-Hakim <yoyohakim615@gmail.com>
2026-06-27 00:07:43 +0300

    certificates style changing

--- script.js
+++ script.js
@@ -19,10 +19,14 @@ function showContent(sectionId) {
     }
   }
 }
+let certificateInterval;
 document.addEventListener("DOMContentLoaded", function () {
-  setInterval(changeCertificate, 6000);
+  startCertificateInterval();
 });
-
+function startCertificateInterval() {
+  clearInterval(certificateInterval);
+  certificateInterval = setInterval(changeCertificate, 5000);
+}
 function download(app) {
   const link = document.createElement("a");
   link.href = app;
@@ -69,9 +73,27 @@ function changeCertificate() {
 
     currentCertificate = (currentCertificate + 1) % certificates.length;
 
-    certificates[currentCertificate].style.display = "inline-block";
+    certificates[currentCertificate].style.display = "inline-flex";
 
     certificates[currentCertificate].style.animation =
       "comeFromRight 0.3s ease-out forwards";
+  }, 300);
+}
+function changeCertificateBack() {
+  certificates[currentCertificate].style.animation =
+    "goRight 0.3s ease-out forwards";
+
+  setTimeout(() => {
+    certificates[currentCertificate].style.display = "none";
+    if (currentCertificate == 0) {
+      currentCertificate = certificates.length - 1;
+    } else {
+      currentCertificate = currentCertificate - 1;
+    }
+
+    certificates[currentCertificate].style.display = "inline-flex";
+
+    certificates[currentCertificate].style.animation =
+      "comeFromLeft 0.3s ease-out forwards";
   }, 300);
 }
